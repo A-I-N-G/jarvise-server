c64bec047451720fe6575397d1aa3b154185bc45
aengun <aengun@icloud.com>
2023-05-28 21:56:54 +0900

    chore: swagger 설정

--- src/main/java/com/hyundaiite/jarvis/config/SwaggerConfig.java
+++ src/main/java/com/hyundaiite/jarvis/config/SwaggerConfig.java
@@ -2,47 +2,33 @@ package com.hyundaiite.jarvis.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableWebMvc
+@EnableSwagger2
 public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(true) // Swagger 에서 제공해주는 기본 응답 코드를 표시할 것이면 true
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hyundaiite.jarvis")) // Controller가 들어있는 패키지. 이 경로의 하위에 있는 api만 표시됨.
+                .paths(PathSelectors.any()) // 위 패키지 안의 api 중 지정된 path만 보여줌. (any()로 설정 시 모든 api가 보여짐)
+                .build();
+    }
 
-//    private ApiInfo swaggerInfo() {
-//        return new ApiInfoBuilder().title("IoT API")
-//                .description("IoT API Docs").build();
-//    }
-//
-//    @Bean
-//    public Docket swaggerApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .consumes(getConsumeContentTypes())
-//                .produces(getProduceContentTypes())
-//                .apiInfo(swaggerInfo()).select()
-//                .apis(
-//                        RequestHandlerSelectors.basePackage("kr.pe.skyer9.warehouse.item.web")
-//                                .or(RequestHandlerSelectors.basePackage("kr.pe.skyer9.warehouse.ipchulgo.web"))
-//                )
-//                .paths(PathSelectors.any())
-//                .build()
-//                .useDefaultResponseMessages(false);
-//    }
-//
-//    private Set<String> getConsumeContentTypes() {
-//        Set<String> consumes = new HashSet<>();
-//        consumes.add("application/json;charset=UTF-8");
-//        consumes.add("application/x-www-form-urlencoded");
-//        return consumes;
-//    }
-//
-//    private Set<String> getProduceContentTypes() {
-//        Set<String> produces = new HashSet<>();
-//        produces.add("application/json;charset=UTF-8");
-//        return produces;
-//    }
-//
-//    @Bean
-//    public InternalResourceViewResolver defaultViewResolver() {
-//        return new InternalResourceViewResolver();
-//    }
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Jarvis Rest API Documentation")
+                .description("A-I-N-G!!")
+                .version("0.1")
+                .build();
+    }
 }
